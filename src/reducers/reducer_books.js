8e7d5afec0d24dd212384ed0c10d4fe1ee685938
Aaron Arinder <aaronarinder@gmail.com>
2017-09-26 14:21:04 -0400

    adding active book reducer and action

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    { title: 'Harry Potter' },
-    { title: 'Probability Moon' },
-    { title: 'Bible' },
-    { title: 'Petitionary Prayer' }
+    { title: 'Harry Potter', pages: 101 },
+    { title: 'Probability Moon', pages: 202 },
+    { title: 'Bible', pages: 303 },
+    { title: 'Petitionary Prayer', pages: 404 }
   ];
 };
